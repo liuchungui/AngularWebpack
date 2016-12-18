cfdfec9142947c79dfdc7aad99f607dd9a40159a
liuchungui <404468494@qq.com>
2016-12-18 17:27:18 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -3,31 +3,38 @@
  */
 const webpack = require("webpack");
 
-module.exports = {
+var ENV = process.env.ENV_MODE;
+var isProd = ENV == 'production';
+
+module.exports = function makeWebpackConfig() {
+    var config = {};
+
     //入口文件
-    entry: {
+    config.entry = {
         demoApp: "./demoApp.js",
         vendor: "./vendor.js"
-    },
-    /**
-     * Devtool配置
-     */
-    devtool: 'source-map',
+    };
 
-    //生成的文件配置
-    output: {
+    config.output = {
         //生成文件的路径,__dirname是当前项目路径,与webpack.config.js同级
-        path: __dirname,
+        path: isProd ? __dirname + '/dist' : __dirname,
         //文件名
         filename: "[name].bundle.js"
-    },
-    module: {
+    };
+
+    config.module = {
         //将css文件打包进去
         loaders: [
             { test: /\.css$/, loader: "style!css" }
         ]
-    },
-    plugins: [
+    };
+
+    /**
+     * Devtool配置
+     */
+    config.devtool = 'source-map';
+
+    config.plugins = [
         /**
          * 此插件会自动加载jquery,解决jquery无法引用的问题
          */
@@ -39,5 +46,8 @@ module.exports = {
         new webpack.DefinePlugin({
             ENV_MODE: JSON.stringify(process.env.ENV_MODE),
         })
-    ]
-};
+    ];
+
+    return config;
+
+}();
